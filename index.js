afcd00ecf5e5b49321725567e0545b6d94b5d096
Mu-An Chiou <me@muanchiou.com>
2019-11-13 16:53:04 -0800

    Fix visible check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let map = ""
 
 for(const heading of document.querySelectorAll('h1, h2, h3, h4, h5, h6')) {
   const text = getText(heading)
-  if (heading.clientHeight > 0 && text) { // visible
+  if ((heading.offsetHeight > 0 || heading.offsetWidth) && text) { // visible
     const n = parseInt(heading.tagName.match(/\d/)[0])
     map += (new Array((n-1)*2)).fill("-").join("") + heading.tagName.toLowerCase() + ": " + text + "\n"
   }
